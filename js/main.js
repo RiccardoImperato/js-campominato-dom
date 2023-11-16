5d3c17199090949780f1338628469494d05d953f
RiccardoImperato <riccardo.imperato@libero.it>
2023-11-16 16:45:24 +0100

    feat: bombe nella griglia

--- js/main.js
+++ js/main.js
@@ -11,9 +11,6 @@ function myCreateElement(tag, className, content) {
 /*/ Funzioni */
 
 // Programma 
-
-// Bombe
-const listaBombe = [];
 // Select
 const selettoreDifficolta = document.querySelector('.select');
 // Bottone play
@@ -23,27 +20,31 @@ const board = document.querySelector('.board');
 
 // Click bottone
 playBtn.addEventListener("click", function () {
+    // Array bombe vuoto
+    const listaBombe = [];
     board.innerHTML = '';
     let dimensioneGriglia = 0;
     //Selezione difficoltà
     let difficolta = selettoreDifficolta.value;
     console.log(`Difficoltà: ${difficolta}`);
 
-    if (difficolta === 'difficile') {
+    if (difficolta === 'facile') {
         dimensioneGriglia = 100;
     }
     if (difficolta === 'normale') {
         dimensioneGriglia = 81;
     }
-    if (difficolta === 'facile') {
+    if (difficolta === 'difficile') {
         dimensioneGriglia = 49;
     }
 
     // numeri delle bombe generate in base alla difficoltà
     for (let i = 1; i <= 16; i++) {
         let bombe = Math.floor(Math.random() * dimensioneGriglia);
-        console.log(bombe);
+        listaBombe.push(bombe);
     }
+    // Array bombe pieno
+    console.log('Bombe: ' + listaBombe);
 
     // Generazione griglia
     for (let i = 1; i <= dimensioneGriglia; i++) {
@@ -64,24 +65,12 @@ playBtn.addEventListener("click", function () {
         myElement.addEventListener("click", function () {
             myElement.classList.add('active');
             console.log('Cella', i);
+            if (listaBombe.includes(i)) {
+                myElement.classList.add('bomba');
+                console.log('Bomba');
+            }
         })
         board.append(myElement);
     }
 });
 
-/*
-
-Partendo dall'esercizio precedente aggiungo la logica del gioco.
-Comincio con un array vuoto (lista bombe) con lunghezza di 16 elementi e creo una funzione che genera 16 numeri casuali (bombe), in base alla difficoltà prescelta: 
-
-- Numeri da 1 a 49 a difficoltà difficile
-- Numeri da 1 a 81 a difficoltà normale
-- Numeri da 1 a 100 a difficoltà facile
-
-Uso un ciclo while per non generare le stesso numero/bomba più di una volta.
-Quando l'utente clicca su play per generare la griglia, richiamo la funzione che genera numeri casuali (bombe) e li pusho nell'array (lista bombe). 
-Aggiungo un evento al click sulle celle, per cui se il numero della cella è incluso nella lista dei numeri generati (lista bombe), le cella si colora di rosso e la partita termina, altrimenti, la cella si colora di azzurro come in precedenza e la partita prosegue.
-La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
-Al termine della partita il programma comunica il punteggio, rappresentato dal numero di volte che l’utente ha cliccato su una cella che non era una bomba.
-
-*/
